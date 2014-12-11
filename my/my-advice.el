7b6ce16933c527b29f693cc8a44b41070cca90df
bion <bion@substantial.com>
2014-12-10 16:19:56 -0800

    Fix make-term for rvm

--- my/my-advice.el
+++ my/my-advice.el
@@ -1,1 +1,12 @@
+(defadvice make-term (around force-interactive-zsh activate)
+  "Force make-term to pass interactive flag to zsh."
+  (if (and
+
+       (= (string-match "zsh" (ad-get-arg 1)))
+       (not (ad-get-args 3)))
+      (ad-set-args 3 '("--interactive" "--login")))
+
+  (print (ad-get-args 3))
+  ad-do-it)
+
 (provide 'my-advice)
