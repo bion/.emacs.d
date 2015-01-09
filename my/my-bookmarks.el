bc048ad1c0434bdfbdae8956b0f9cc769733bb62
bion <bion@substantial.com>
2015-01-09 08:40:45 -0800

    Update my-bookmarks

--- my/my-bookmarks.el
+++ my/my-bookmarks.el
@@ -8,40 +8,25 @@
  (rear-context-string . "16 Aug 21 12:39 ")
  (position . 513))
 ("thoughtstream-chef"
- (filename . "~/code/thoughtstream-chef/")
+ (filename . "~/dev/thoughtstream-chef/")
  (front-context-string . ".chef\n  -rwxrwxr")
  (rear-context-string . "96 Jul 31 10:50 ")
  (position . 225))
 ("thoughtstream-collaboration_service"
- (filename . "~/code/thoughtstream-collaboration_service/")
+ (filename . "~/bluescape/thoughtstream-collaboration_service/")
  (front-context-string . ".bundle\n  -rw-rw")
  (rear-context-string . "96 Aug  5 11:00 ")
  (position . 239))
 ("thoughtstream-user_management"
- (filename . "~/code/thoughtstream-user_management/")
+ (filename . "~/bluescape/thoughtstream-user_management/")
  (front-context-string . "app\n  drwxrwxr-x")
  (rear-context-string . "96 Jul 23 10:42 ")
  (position . 233))
 ("thoughtstream-browser_client"
- (filename . "~/code/thoughtstream-browser_client/")
+ (filename . "~/bluescape/thoughtstream-browser_client/")
  (front-context-string . "app\n  -rw-rw-r--")
  (rear-context-string . "96 Apr  2 10:43 ")
  (position . 236))
-("simp"
- (filename . "~/code/simp/")
- (front-context-string)
- (rear-context-string . "th-bookmarks.el\n")
- (position . 626))
-("user-portal"
- (filename . "~/code/user-portal/")
- (front-context-string . "app\n  -rw-r--r--")
- (rear-context-string . "012-10-24 10:09 ")
- (position . 231))
-("crushee"
- (filename . "~/code/crushee/")
- (front-context-string . "app\n  drwxr-xr-x")
- (rear-context-string . "012-02-08 18:16 ")
- (position . 241))
 (".emacs.d"
  (filename . "~/.emacs.d/")
  (front-context-string . "auto-save-list\n ")
